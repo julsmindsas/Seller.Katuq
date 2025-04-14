b0115105f865c796cfdbc7a5d03e6d17a541043b
jlbatty <70337462+jlbatty@users.noreply.github.com>
2025-04-13 23:46:05 -0500

    feat: Implement Bodegas management with create, edit, and delete functionalities; enhance UI with form validations and styling

--- src/app/components/inventarios/bodegas/bodegas.component.ts
+++ src/app/components/inventarios/bodegas/bodegas.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {CrearBodegasComponent} from './crear-bodegas/crear-bodegas.component';
 @Component({
   selector: 'app-bodegas',
   templateUrl: './bodegas.component.html',
   styleUrls: ['./bodegas.component.scss']
 })
 export class BodegasComponent implements OnInit {
+ngOnInit(): void {
+  
+}
+  bodegas: any[] = [
+    {
+      id: 1,
+      nombre: 'Bodega Principal',
+      idBodega: 'BOD-001',
+      direccion: 'Calle 123 #45-67',
+      coordenadas: '4.60971, -74.08175',
+      ciudad: 'Bogotá',
+      departamento: 'Bogotá D.C.',
+      pais: 'Colombia',
+      tipo: 'Física'
+    },
+    {
+      id: 2,
+      nombre: 'Bodega Virtual',
+      idBodega: 'BOD-002',
+      direccion: 'N/A',
+      coordenadas: '',
+      ciudad: 'Virtual',
+      departamento: '',
+      pais: '',
+      tipo: 'Transaccional'
+    }
+  ];
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
 
-  ngOnInit(): void {
+  abrirModalCrear() {
+    const modalRef = this.modalService.open(CrearBodegasComponent, { 
+      size: 'lg',
+      centered: true
+    });
+    
+    modalRef.result.then((result) => {
+      if (result) {
+        this.bodegas.push(result); // Agrega la nueva bodega
+      }
+    });
   }
 
+  abrirModalEditar(bodega: any) {
+    const modalRef = this.modalService.open(CrearBodegasComponent, { 
+      size: 'lg',
+      centered: true
+    });
+    
+    modalRef.componentInstance.bodegaData = bodega;
+    modalRef.componentInstance.isEditMode = true;
+    
+    modalRef.result.then((result) => {
+      if (result) {
+        const index = this.bodegas.findIndex(b => b.id === result.id);
+        if (index !== -1) {
+          this.bodegas[index] = result; // Actualiza la bodega
+        }
+      }
+    });
+  }
+
+  eliminarBodega(bodega: any) {
+    this.bodegas = this.bodegas.filter(b => b.id !== bodega.id);
+  }
 }
--- src/app/components/inventarios/bodegas/crear-bodegas/crear-bodegas.component.ts
+++ src/app/components/inventarios/bodegas/crear-bodegas/crear-bodegas.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-crear-bodegas',
   templateUrl: './crear-bodegas.component.html',
@@ -7,9 +8,98 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearBodegasComponent implements OnInit {
 
-  constructor() { }
+  @Input() bodegaData: any;
+  @Input() isEditMode = false;
+
+  bodegaForm: FormGroup;
+  tiposBodega = ['Física', 'Transaccional'];
+  paises = ['Colombia', 'Ecuador', 'Perú', 'Chile', 'Argentina'];
+  departamentos: string[] = [];
+  ciudades: string[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    public activeModal: NgbActiveModal
+  ) {
+    this.bodegaForm = this.fb.group({
+      id: [''],
+      nombre: ['', Validators.required],
+      idBodega: ['', [Validators.required, Validators.pattern(/^BOD-[A-Z0-9]{3,}$/)]],
+      direccion: [''],
+      coordenadas: [''],
+      ciudad: [''],
+      departamento: [''],
+      pais: [''],
+      tipo: ['Física', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+    if (this.bodegaData) {
+      this.bodegaForm.patchValue(this.bodegaData);
+    }
+
+    this.bodegaForm.get('tipo')?.valueChanges.subscribe(tipo => {
+      this.actualizarValidaciones(tipo);
+    });
+
+    this.bodegaForm.get('pais')?.valueChanges.subscribe(pais => {
+      this.cargarDepartamentos(pais);
+    });
+
+    this.bodegaForm.get('departamento')?.valueChanges.subscribe(depto => {
+      this.cargarCiudades(depto);
+    });
+  }
+
+  actualizarValidaciones(tipo: string) {
+    const controles = ['direccion', 'coordenadas', 'ciudad', 'departamento', 'pais'];
+    
+    controles.forEach(control => {
+      const formControl = this.bodegaForm.get(control);
+      if (tipo === 'Física') {
+        formControl?.setValidators([Validators.required]);
+      } else {
+        formControl?.clearValidators();
+        formControl?.setValue('');
+      }
+      formControl?.updateValueAndValidity();
+    });
+  }
+
+  cargarDepartamentos(pais: string) {
+    // Simulación de carga de departamentos
+    this.departamentos = pais === 'Colombia' ? 
+      ['Bogotá D.C.', 'Antioquia', 'Valle del Cauca'] : 
+      ['Departamento 1', 'Departamento 2'];
+    this.bodegaForm.get('departamento')?.setValue('');
+  }
+
+  cargarCiudades(departamento: string) {
+    // Simulación de carga de ciudades
+    this.ciudades = departamento === 'Bogotá D.C.' ? 
+      ['Bogotá'] : 
+      ['Ciudad 1', 'Ciudad 2'];
+    this.bodegaForm.get('ciudad')?.setValue('');
+  }
+
+  guardarBodega() {
+    if (this.bodegaForm.invalid) {
+      this.marcarControlesComoTocados();
+      return;
+    }
+
+    this.activeModal.close(this.bodegaForm.value);
+  }
+
+  cancelar() {
+    this.activeModal.dismiss();
+  }
+
+  private marcarControlesComoTocados() {
+    Object.values(this.bodegaForm.controls).forEach(control => {
+      control.markAsTouched();
+    });
   }
 
 }
