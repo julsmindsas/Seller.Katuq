55401ea9faa60be82cd6f480e9c9749c5529a6d6
julsmindsas <dgarciah@julsmind.com>
2025-04-14 20:31:24 -0500

    feat: Actualizar versión a 8.5.0 en archivos de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: "8.4.9 - 08 de Abril 2025 (Beta)",
+  version: "8.5.0 - 14 de Abril 2025 (Beta)",
   firebase: {
     apiKey: "AIzaSyAmAnBBefe_f6rwSLIUK0e1JexuDGP2w_4",
     authDomain: "julsmind-katuq.firebaseapp.com",
