2bcf091091908a574699615a65fbd1e44282afe7
julsmindsas <dgarciah@julsmind.com>
2025-05-31 11:47:10 -0500

    chore(environment): actualizar la versión en los archivos de entorno a "2025.05.31.1 - 31 de Mayo 2025 (Beta)".

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: "2025.05.27.5 - 27 de Mayo 2025 (Beta)",
+  version: "2025.05.31.1 - 31 de Mayo 2025 (Beta)",
   firebase: {
     apiKey: "AIzaSyAmAnBBefe_f6rwSLIUK0e1JexuDGP2w_4",
     authDomain: "julsmind-katuq.firebaseapp.com",
