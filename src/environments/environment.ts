ab40e4a56efbe657926f0e7388f6646a944e9fc5
julsmindsas <dgarciah@julsmind.com>
2025-06-13 16:28:52 -0500

    mejoras
    dasdad

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  version: "2025.06.13.6 - 13 de Junio 2025 (Beta)",
+  version: "2025.06.13.7 - 13 de Junio 2025 (Beta)",
   firebase: {
     apiKey: "AIzaSyAmAnBBefe_f6rwSLIUK0e1JexuDGP2w_4",
     authDomain: "julsmind-katuq.firebaseapp.com",
