840355676bdc7c084a833ca448e3e81d83278e56
julsmindsas <dgarciah@julsmind.com>
2025-06-18 08:48:14 -0500

    Refactoriza el componente de información de la empresa
    - Actualiza las rutas de importación para los servicios y modelos.
    - Agrega el servicio de autenticación para manejar el cierre de sesión si no hay logo disponible.
    - Mejora la lógica de suscripción para obtener la información de la empresa, asegurando que se manejen correctamente los casos nulos.

--- src/app/shared/components/header/elements/company-information/company-information.component.ts
+++ src/app/shared/components/header/elements/company-information/company-information.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { CompanyInformation } from 'src/app/shared/models/User/CompanyInformation';
-import { SecurityService } from 'src/app/shared/services/security/security.service';
+import { CompanyInformation } from '../../../../models/User/CompanyInformation';
+import { SecurityService } from '../../../../services/security/security.service';
+import { AuthService } from '../../../../services/firebase/auth.service';
 
 @Component({
     selector: 'app-company-information',
-    templateUrl: 'company-information.component.html',
+    templateUrl: './company-information.component.html',
     styleUrls: ['./company-information.component.scss']
 })
 
 export class CompanyInformationComponent implements OnInit {
     constructor(
-        private securityService: SecurityService
+        private securityService: SecurityService,
+        private authService: AuthService
     ) { }
 
     companyInformation: CompanyInformation;
-
     ngOnInit() {
-        this.securityService.getCompanyInformationLogged$().subscribe((companyInformation: CompanyInformation) => {
+        this.securityService.getCompanyInformationLogged$().subscribe((companyInformation: CompanyInformation | null) => {
             if (!companyInformation) {
                 companyInformation = this.securityService.getCompanyInformationLogged();
             }
-            this.companyInformation = companyInformation;
+            else if (!companyInformation?.imgUrlLogo) {
+                this.authService.SignOut();
+            }
+            this.companyInformation = companyInformation as CompanyInformation;
         });
     }
 }
