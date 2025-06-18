e51cce8a47930b6d96a38e3a3ac5ee0e390d8de2
julsmindsas <dgarciah@julsmind.com>
2025-06-17 21:13:50 -0500

    Actualiza la versión de la aplicación a 2025.06.17.1 en los entornos de producción y desarrollo.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  version: "2025.06.16.3 - 16 de Junio 2025 (Beta)",
+  version: "2025.06.17.1 - 17 de Junio 2025 (Beta)",
   firebase: {
     apiKey: "AIzaSyAmAnBBefe_f6rwSLIUK0e1JexuDGP2w_4",
     authDomain: "julsmind-katuq.firebaseapp.com",
