2d1c63cef6a1a2436029d80a529f0bd728328f28
jlbatty <70337462+jlbatty@users.noreply.github.com>
2025-08-17 00:47:15 -0500

    Actualiza el cálculo del subtotal en el componente de lista de pedidos para incluir el costo de envío. Se añade la función getSubtotalConDomicilio para calcular el subtotal de un pedido individual. Se actualiza la versión en los archivos de entorno a 2025.08.17.1.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: "2025.08.16.6 - 16 de Agosto 2025 (Beta)",
+  version: "2025.08.17.1 - 17 de Agosto 2025 (Beta)",
   firebase: {
     apiKey: "AIzaSyAmAnBBefe_f6rwSLIUK0e1JexuDGP2w_4",
     authDomain: "julsmind-katuq.firebaseapp.com",
