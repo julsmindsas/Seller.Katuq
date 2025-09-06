cf058b42b5ec80128af7748036315a2a25a1b003
Julsmind S.A.S <117603193+julsmindsas@users.noreply.github.com>
2025-09-06 11:42:19 -0500

    Remove estado column from ordenes despacho table
    Update version to 2025.09.06.3 in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: "2025.09.06.1 - 6 de Septiembre 2025 (Beta)",
+  version: "2025.09.06.3 - 6 de Septiembre 2025 (Beta)",
   firebase: {
     apiKey: "AIzaSyAmAnBBefe_f6rwSLIUK0e1JexuDGP2w_4",
     authDomain: "julsmind-katuq.firebaseapp.com",
