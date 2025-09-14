44a28d383885292157122cc8afaed67e04e8edc3
Julsmind S.A.S <117603193+julsmindsas@users.noreply.github.com>
2025-09-14 11:47:21 -0500

    Add loader animation and improve SVG styling

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { LoaderService } from '../../shared/services/loader.service';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from "@angular/animations";
+import { LoaderService } from "../../shared/services/loader.service";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-loader-custom',
-  templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss']
+  selector: "app-loader-custom",
+  templateUrl: "./loader.component.html",
+  styleUrls: ["./loader.component.scss"],
+  animations: [
+    trigger("loaderAnimation", [
+      state(
+        "void",
+        style({
+          opacity: 0,
+          transform: "scale(0.95)",
+        }),
+      ),
+      transition("void <=> *", [animate("200ms ease-in-out")]),
+    ]),
+  ],
 })
 export class LoaderComponent implements OnInit {
   isLoading$: Observable<boolean>;
 
-  constructor(private loaderService: LoaderService) { }
+  constructor(private loaderService: LoaderService) {}
 
   ngOnInit(): void {
     this.isLoading$ = this.loaderService.loading$;
