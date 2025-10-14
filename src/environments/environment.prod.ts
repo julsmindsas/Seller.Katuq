18c98b6903ffcb3ed36c81c357cd6e7c59a18383
julsmindsas <117603193+julsmindsas@users.noreply.github.com>
2025-10-14 13:03:21 -0500

    feat: Reemplazar filtros en el componente de despachos por un componente compartido
    - Se elimina la implementación anterior de filtros en el componente `DespachosComponent` y se integra un nuevo componente `app-shared-filters`, que centraliza la lógica de filtrado y mejora la reutilización del código.
    - Se actualiza la versión en los archivos de entorno a 2025.10.14.1, reflejando la última actualización del sistema.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: "2025.10.13.1 - 13 de Octubre 2025 (Beta)",
+  version: "2025.10.14.1 - 14 de Octubre 2025 (Beta)",
   firebase: {
     apiKey: "AIzaSyAmAnBBefe_f6rwSLIUK0e1JexuDGP2w_4",
     authDomain: "julsmind-katuq.firebaseapp.com",
