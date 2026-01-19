5b02caf5b63e2ec23d3de2588d84209384df8081
julsmindsas <117603193+julsmindsas@users.noreply.github.com>
2026-01-19 10:34:40 -0500

    Update environment version to 2026.01.19.2 for consistency across production and development environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: "2026.01.19.1 - 19 de Enero 2026 (Beta)",
+  version: "2026.01.19.2 - 19 de Enero 2026 (Beta)",
   firebase: {
     apiKey: "AIzaSyAmAnBBefe_f6rwSLIUK0e1JexuDGP2w_4",
     authDomain: "julsmind-katuq.firebaseapp.com",
